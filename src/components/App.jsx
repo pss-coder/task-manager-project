1f9634fa1bcf5f64f30e3aab3cfca62a06f6be6b
Pawandeep Singh <pawandeepss@u.nus.edu>
2024-04-07 14:13:13 +0800

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import Footer from './Footer'
+import Header from './Header'
+
+export default function App() {
+
+
+
+  return (
+    <>
+      <div>
+
+       <Header /> 
+
+        <div className="py-10">
+          <header>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">All Tasks</h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
